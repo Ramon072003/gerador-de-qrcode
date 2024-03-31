1867ebcc865afd02d903aab80bce2cfa770900c8
Ramon Oliveira Silva <89915603+Ramon072003@users.noreply.github.com>
2024-03-31 10:58:14 -0300

    open-image

--- geradorQrcode.py
+++ geradorQrcode.py
@@ -1,16 +1,19 @@
 import qrcode
 import qrcode.image.svg
 from qrcode.image.pure import PyPNGImage
+from PIL import Image
 
 class Qrcode:
     def __init__(self):
         pass
 
-    def salvarPng(self,link,nome='Qrcode.png'):
+    def salvarPng(self,link,nome):
         url = qrcode.make(link, image_factory=PyPNGImage)
         url.save(nome)
+        imagem = Image.open(nome)
+        imagem.show()
 
-    def salvarSVG(self,link,nome='Qrcode.svg'):
+    def salvarSVG(self,link,nome):
         factory = qrcode.image.svg.SvgImage
         url = qrcode.make(link,image_factory=factory)
         url.save(nome)
